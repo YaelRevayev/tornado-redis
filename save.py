94d2458e96667338cb9a921f6289d0e8579d4899
YaelRevayev <gexploreapp@gmail.com>
2024-03-04 19:28:29 +0200

    testing on vm

--- save.py
+++ save.py
@@ -1,7 +1,7 @@
 import redis
 
 def save_key_value_pair(dict):
-    redis_client = redis.Redis(host='172.214.74.29', port=6379, db=0)
+    redis_client = redis.Redis(host='localhost',port=6379)
 
     for key in dict:
         redis_client.set(key,dict[key])
